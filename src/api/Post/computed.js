6dcebcd1b862c48ec6d40e0384bc9428bdb2422d
bees1114 <bees1114@naver.com>
2019-04-21 17:30:39 +0900

    add api for post, feed

---
+++ src/api/Post/computed.js
@@ -0,0 +1,29 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+    Post: {
+        isLiked: async (parent, _, {request}) => {
+            const { user } = request;
+            const { id } = parent;
+            return prisma.$exists.like({
+                AND: [{
+                    user: {
+                        id:user.id
+                    },
+                    post: {
+                        id:id
+                    }
+                }]
+            })
+        },
+        likeCount: async (parent) => {
+            const { id } = parent
+            const likeCount = await prisma.likesConnection({
+                where: {post: { id } }
+            })
+            .aggregate()
+            .count();
+            return likeCount
+        }
+    }
+};
--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,28 +1,13 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGMENT } from "../../../fragments";
+import { COMMENT_FRAGMENT, USER_FRAGMENT, FULL_POST_FRAGMENT } from "../../../fragments";
 
 
 export default {
     Query: {
         seeFullPost: async(_, args) => {
             const { id } = args;
-            const post = await prisma.post({id});
-            const comments = await prisma.post({ id })
-            .comments()
-            .$fragment(COMMENT_FRAGMENT);
-            
-            const likeCount = await prisma.likesConnection({
-                where: {post: { id } }
-            })
-            .aggregate()
-            .count();
-            
-            const ret = {
-                post,
-                comments,
-                likeCount
-            }
-            return ret
+            const post = await prisma.post({id}).$fragment(FULL_POST_FRAGMENT);
+            return post
         }
     }
 }
