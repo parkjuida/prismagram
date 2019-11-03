3269ff23437e0c577778371317e56293abfdae4c
bees1114 <bees1114@naver.com>
2019-11-03 13:57:26 +0900

    Change url

--- frontend/src/Apollo/Client.js
+++ frontend/src/Apollo/Client.js
@@ -1,14 +1,16 @@
-
-import ApolloClient from 'apollo-boost';
-import { defaults, resolvers } from './LocalState';
+import ApolloClient from "apollo-boost";
+import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-    uri: "http://localhost:4000",
-    clientState: {
-        defaults:defaults,
-        resolvers:resolvers
-    },
-    headers: {
-        "Authorization": `Bearer ${localStorage.getItem("token")}`
-    }
-})
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "https://parkjuida-prismagram.herokuapp.com/",
+  clientState: {
+    defaults: defaults,
+    resolvers: resolvers
+  },
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`
+  }
+});
